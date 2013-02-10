eadbbee015814f22cf57f308eaa0849e56aedbd5
amrsharaf <amr.nabil.sharaf@gmail.com>
2013-02-10 14:08:50 +0200

    adding new files

--- shapemotiondescriptor/describe_videos.m
+++ shapemotiondescriptor/describe_videos.m
@@ -1,4 +1,4 @@
-function [action] = classify(video)
+function describe_videos()
 	% loading videos
 	addpath('../src');
 	%addpath('../time_warping');
@@ -8,43 +8,28 @@ function [action] = classify(video)
 	% list all video names
 	names = fieldnames(rgb_videos); 
 	videos = struct;
-
+	% for all videos
 	%for i = 1:numel(names)
-	for i = 1:5
+	for i = 1:1
 		% Process a new  video
-		vdes = [];
+		vdes = [];% video descriptor
     		rgb_video = rgb_videos.(names{i});
+		names{i}
 		aligned_video = aligned_videos.(names{i});
 		sz = size(aligned_video);
 		nframes = sz(3);
-		for j = 1: 40: nframes-1
-		%for j = 1:2
+		%for j = 1: 40: nframes-1
+		for j = 1:1
 			[shapedes, motiondes] = extract_features(aligned_video(:,:,j), rgb_video(j).cdata, rgb_video(j+1).cdata);
 			des = [shapedes motiondes];
 			vdes = horzcat(vdes, des');
 		end
+		'setting field'
 		videos = setfield(videos, names{i}, vdes);
-		'done'
+		'done setting field'
+		videos
+		whos videos
 	end
-	
-	%for i = 1:numel(names)
-	for i = 1:1 
-		% Process a new  video
-    		rgb_video = videos.(names{i});
-		for j = 1:numel(names)
-		%for j = 1:1
-			aligned_video = videos.(names{j});
-			% now you have two videos
-			dtw(rgb_video,aligned_video)
-		end
-	end
-	%video = aligned_videos.moshe_run;
-	%video2 = rgb_videos.moshe_run;
-	%sz = size(video);
-	%sz2 = size(video2)
-	% number of frames
-	%nframes = sz(3);
-	%for i = 10 : 10
-	%	[shapedes, motiondes] = extract_features(video(:,:,i), video2(i).cdata, video2(i+1).cdata);
-	%end
+	%save('described_videos.mat','videos');
 end
+
