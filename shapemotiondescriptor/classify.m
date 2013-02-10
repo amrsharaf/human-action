cb9ecf49e320f3d67e6bc9eaa8c5ee22470d262e
amrsharaf <amr.nabil.sharaf@gmail.com>
2013-02-10 17:47:16 +0200

    some updates to code

--- shapemotiondescriptor/classify.m
+++ shapemotiondescriptor/classify.m
@@ -1,8 +1,64 @@
-function [action] = classify(video)
-	% loading videos
-	addpath('../src');
-	%addpath('../time_warping');
-	rgb_videos = videos_loader();
+% Fit a model 
+function [correct_rate] = classify(train, test)
+
+% This m-file implements the approximate median algorithm for background
+% subtraction.  It may be used free of charge for any purpose (commercial
+% or otherwise), as long as the author (Seth Benton) is acknowledged.
+clear all
+Frames = [];
+Training = [];
+Group = [];
+Sample = [];
+Label = [];
+base_dir = '../weizmann_part/videos/';
+activity = {'jump', 'run', 'walk'};
+for c=1:length(activity)
+	for ii=1:3
+		source = aviread(strcat(base_dir,activity{c},int2str(ii)));
+		% process frames
+		Frames = [];
+		for i = 1:length(source)
+		    fr = source(i).cdata;
+		    Frames(:,:,i)=rgb2gray(fr);
+		end
+		c
+		ii
+		'done with video'
+		%[VX, VY] = lk3(Frames);
+		[VX, VY] = OpticalFlow(Frames, 30,1);
+		'done with optical flow'
+		sz = size(VX)
+		for k = 1 : sz(3)
+			figure(1), subplot(2,1,1), imshow(source(k).cdata);
+			subplot(2,1,2);
+			imshow(source(k).cdata);
+			hold on;
+			quiver(-VX(:,:,k),-VY(:,:,k),0);	
+			hold off
+			ohog = gradientHistogram(VX(:,:,k), VY(:,:,k), 32);
+			if ii ~= 3 
+				Training = [Training; ohog'];
+				Group = [Group; c];
+			else
+				Sample = [Sample; ohog'];
+				Label = [Label; c];
+			end
+		end
+	end
+end
+labels = knnclassify(test.data, train.data, train.class);
+cp = classperf(test.class,labels);
+%get(cp)
+cp.CountingMatrix
+table = cp.DiagnosticTable;
+tp = table(1,1)
+fp = table(1,2)
+tn = table(2,1)
+fn = table(2,2)
+precision = tp / (tp + fp) 
+recall = tp / (tp + fn)
+error_rate = cp.ErrorRate
+
 	videos = load('../weizmann/classification_masks.mat');
 	aligned_videos = videos.aligned_masks;
 	% list all video names
